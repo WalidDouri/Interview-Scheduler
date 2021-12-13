eb2e720b05674b19dda1198ea6a7bd01056c3715
Walid <walid_douri@hotmail.com>
2021-12-12 23:20:16 -0500

    Created helper function for appointments & tested it

---
+++ src/helpers/selectors.js
@@ -0,0 +1,28 @@
+
+
+
+export default function getAppointmentsForDay(state, day) {
+  let appointmentsArray = [];
+  const [getAppointmentsForDay] = state.days.filter((data) => data.name === day);
+  // maybe refactor and use map? insted of filter?
+  if (!getAppointmentsForDay) {
+    return [];
+  } else {
+    const appointments = getAppointmentsForDay.appointments.filter(
+      (id) => id === state.appointments[id].id
+    );
+    appointments.forEach((appointment) =>
+      appointmentsArray.push(state.appointments[appointment])
+    );
+    return appointmentsArray;
+  }
+}
+
+
+
+
+
+// function selectUserByName(state, name) {
+//   const filteredNames = state.users.filter(user => user.name === name);
+//   return filteredNames;
+// }
